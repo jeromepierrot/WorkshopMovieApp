3780cf7db6753b628254046dbddcc202eda94cb8
jpierrot <je.pierrot@gmail.com>
2023-04-19 12:21:40 +0200

    add: create 'hugeMovielist' to scroll into the RecyclerView

--- app/src/main/java/com/stonwewater/workshopmovieapp/MainActivity.kt
+++ app/src/main/java/com/stonwewater/workshopmovieapp/MainActivity.kt
@@ -19,8 +19,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_movie)
-        val adapter = MovieAdapter(this, movieList)
+        val adapter = MovieAdapter(this, getHugeMovieList())
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun getHugeMovieList(): List<Movie> {
+        var i = 0
+        val list = mutableListOf<Movie>()
+        while (i <= 100){
+            list.addAll(
+                movieList
+            )
+            i++
+        }
+        list.add(
+            Movie("Fin de liste", android.R.drawable.ic_lock_lock)
+        )
+
+        return list
     }
 }
